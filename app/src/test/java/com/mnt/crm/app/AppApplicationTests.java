3f1c2337d1d950427a9517a805442fcf06104363
mindnervestech <jagbir.paul@gmail.com>
2020-05-05 00:09:54 +0530

    initial commit

---
+++ app/src/test/java/com/mnt/crm/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnt.crm.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
